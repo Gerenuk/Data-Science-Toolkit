f42d72d0ef50a9e12185a61421b0fddc9fc80562
Anton Suchaneck <a.suchaneck@gmail.com>
2018-12-06 13:58:50 +0100

    .

--- dstk.dev/hierarchical_excel_table_reader.py
+++ dstk.dev/hierarchical_excel_table_reader.py
@@ -1,6 +1,7 @@
+import xlwings as xw
 import pandas as pd
 import itertools
-import xlwings as xw
+import re
 
 
 def val_empty(val):
@@ -8,12 +9,6 @@ def val_empty(val):
 
 
 def table_values_to_dataframe(table_values, num_header_rows, num_index_cols):
-    """
-    TODO:
-    * Custom index names (mapping)
-    * Warning on empty row/cols
-    * Header/Index may be detached from data block
-    """
     num_cols = len(table_values[0])
     num_rows = len(table_values)
 
@@ -39,16 +34,23 @@ def table_values_to_dataframe(table_values, num_header_rows, num_index_cols):
     col_index_values = [row[num_index_cols:] for row in table_values[:num_header_rows]]
     col_index_default_names = (f"col{i}" for i in itertools.count(1))
     col_index_names = [
-                          table_values[i][num_index_cols - 1] for i in range(num_header_rows - 1)
-                      ] + [
-                          None
-                      ]  # columns cannot have a name for the deepest
+        table_values[i][num_index_cols - 1] for i in range(num_header_rows - 1)
+    ] + [
+        None
+    ]  # columns cannot have a name for the deepest
     index_names = [
         name if not val_empty(name) else default_name
         for name, default_name in zip(col_index_names, col_index_default_names)
     ]
 
-    col_index = pd.MultiIndex.from_arrays(col_index_values, names=index_names) if index_names else None
+    if index_names is None:
+        col_index=None
+    elif len(col_index_values)>=2:
+        col_index = pd.MultiIndex.from_arrays(col_index_values, names=index_names)
+    elif len(col_index_values)==1:
+        col_index = pd.Index(col_index_values[0], names=index_names)
+    else:
+        raise ValueError(f"{col_index_values}")
 
     row_index_values = [
         [row[ncol] for row in table_values[num_header_rows:]]
@@ -62,7 +64,11 @@ def table_values_to_dataframe(table_values, num_header_rows, num_index_cols):
         name if not val_empty(name) else default_name
         for name, default_name in zip(row_index_names, row_index_default_names)
     ]
-    row_index = pd.MultiIndex.from_arrays(row_index_values, names=index_names) if index_names else None
+    row_index = (
+        pd.MultiIndex.from_arrays(row_index_values, names=index_names)
+        if index_names
+        else None
+    )
 
     df = pd.DataFrame(
         [row[num_index_cols:] for row in table_values[num_header_rows:]],
@@ -80,7 +86,7 @@ def table_values_to_dataframe(table_values, num_header_rows, num_index_cols):
 
 
 def read_excel(
-        excel_name, sheet_name, table_excel_range, num_header_rows=1, num_index_cols=0
+    excel_name, sheet_name, table_excel_range, num_header_rows=1, num_index_cols=0
 ):
     wb = xw.Book(excel_name)
     sht = wb.sheets[sheet_name]
@@ -92,3 +98,109 @@ def read_excel(
         table_values, num_header_rows=num_header_rows, num_index_cols=num_index_cols
     )
     return res
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    #from sqlalchemy import create_engine    # Only for writing to a database
+
+    data_dir = Path(r"E:\Usecases\uc150420\Daten VWN")
+    #engine = create_engine(
+    #    "postgresql://uc150420:T#Cross19@postgres9.vwdl.lan:5432/usecases"
+    #)
+
+    htg_dfs = []
+
+    for file_basename, table_excel_range in [
+        ("Kopie von Potenzialtool_HTGs_DE_20181022.xlsm", "G15:DZ1407"),
+        ("Kopie von Potenzialtool_HTGs_EUR_20181022.xlsm", "G15:DZ10137"),
+        ("Kopie von Potenzialtool_HTGs_INT_20181022.xlsm", "G15:DZ12396"),
+    ]:
+        file_name = data_dir.joinpath("HTG 2012-18", file_basename)
+
+        # Read in Excel to a proper dataframe with (multi) indices
+        carpark_df, corner_cells = read_excel(
+            excel_name=str(file_name),
+            sheet_name="Table",
+            table_excel_range=table_excel_range,
+            num_header_rows=3,
+            num_index_cols=4,
+        )
+
+        # Reshape
+        df2 = (
+            carpark_df["Vor NR"]
+            .stack("Kalenderjahr")
+            .reset_index()
+            .rename(columns={"idx2": "Land", "idx4": "HTG"})
+        )
+
+        htg_dfs.append(df2)
+
+    htg_df = pd.concat(htg_dfs)
+
+    htg_df.columns = htg_df.columns.map(lambda x: re.sub(r"\(|\)", "", x))
+    htg_df.reset_index(drop=True, inplace=True)
+    htg_df = htg_df.query("Kalenderjahr!='Gesamtergebnis'")
+    htg_df["Kalenderjahr"] = htg_df["Kalenderjahr"].apply(pd.to_numeric)
+    htg_df["BG / Umsatz VWN"] = htg_df["BG / Umsatz VWN"].apply(
+        pd.to_numeric, errors="coerce"
+    )
+    for col in ["Marktgebiet", "Land", "Homogene Gruppe A", "HTG"]:
+        htg_df[col] = htg_df[col].astype("category")
+
+    htg_df.to_pickle(data_dir.joinpath("umsatz_anzahl.pkl"))
+
+    file_basename = "Car Parc PR67.0.xlsm"
+    table_excel_range = "A2:AB12627"
+    file_name = data_dir.joinpath("Carpark 2011-18", file_basename)
+
+    carpark_df, corner_cells = read_excel(
+        excel_name=str(file_name),
+        sheet_name="Roh-Daten",
+        table_excel_range=table_excel_range,
+        num_header_rows=1,
+        num_index_cols=0,
+    )
+
+    carpark_df.columns = carpark_df.columns.map("_".join)  # to remove unneed multiindex
+    carpark_df.reset_index(drop=True, inplace=True)
+
+    for col in ["MARKT_NAME", "MARKE", "FZG_KLASSE"]:
+        carpark_df[col] = carpark_df[col].astype("category")
+
+    carpark_df["JAHR"] = carpark_df["JAHR"].apply(pd.to_numeric)
+
+    carpark_df.to_pickle(data_dir.joinpath("carpark.pkl"))
+
+    carpark_df.rename(
+        columns={"MARKT_NAME": "land", "JAHR": "jahr", "Gesamt": "carpark"}, inplace=True
+    )
+
+    htg_df1 = htg_df.rename(
+        columns={
+            "Umsatz VWN": "umsatz",
+            "Alternative Menge VWN": "menge",
+            "Homogene Gruppe A": "htg_name",
+            "HTG": "htg",
+            "Land": "land",
+            "Kalenderjahr": "jahr",
+        }
+    )
+
+    htg_df1 = htg_df1.query(
+        "htg_name!='Ergebnis' and htg!='#' and jahr!='Gesamtergebnis'"
+    )
+
+    htg_df1.to_pickle("umsatz_anzahl_clean.pkl")
+
+    carpark_land = (
+        carpark_df.groupby(["land", "jahr"], as_index=False)
+        .sum()
+    )
+
+    htg_df2 = htg_df1.merge(carpark_land[["land", "jahr", "carpark"]], how="left")
+
+    htg_df2["land"] = htg_df2["land"].astype("category")
+
+    htg_df2.to_pickle(data_dir.joinpath("umsatz_menge_carpark_clean.pkl"))
