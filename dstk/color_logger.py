7779aaa349a766546b459707d501d6fe2264d5d4
Anton Suchaneck <a.suchaneck@gmail.com>
2019-01-04 16:02:23 +0100

    .

--- dstk/color_logger.py
+++ dstk/color_logger.py
@@ -3,12 +3,12 @@ import logging
 import colorlog
 
 
-def create_color_logger(
+def color_logger(
     file=sys.stdout,
     name=None,
     level="INFO",
-    log_format="%(log_color)s[%(levelname)s]%(white)s %(asctime)s %(name)s:%(funcName)s(L%(lineno)s)%(reset)s %(message)s",
-    date_format="%H:%M",
+    log_format="%(log_color)s[%(levelname)s]%(white)s %(asctime)s %(funcName)s(L%(lineno)s)%(reset)s: %(message)s", # %(name)s (e.g. root)
+    date_format="%H:%M:%S", # %d.%m.%y
 ):
     logger = logging.getLogger(name)
     logger.setLevel(level)
--- dstk/timer.py
+++ dstk/timer.py
@@ -1,4 +1,22 @@
 import time
+from dataclasses import dataclass
+
+
+@dataclass
+class TimerDuration:
+    value: float
+
+    def __str__(self):
+        duration_seconds = self.value
+
+        if duration_seconds < 1:
+            duration_text = f"{duration_seconds*1000:.0f}msec"
+        elif duration_seconds < 60:
+            duration_text = f"{duration_seconds:.1f}sec"
+        else:
+            duration_text = f"{duration_seconds/60:.1f}min"
+
+        return duration_text
 
 
 class Timer:
@@ -6,27 +24,23 @@ class Timer:
         self.start_time = time.time()
 
     def duration(self):
-        return time.time() - self.start_time
+        return TimerDuration(value=time.time() - self.start_time)
 
-    def __str__(self, dynamic_units=True):
+    def __repr__(self):
+        return f"Timer()"  # repr in debugging with time not meaningful
+
+    def __str__(self):
         duration = self.duration()
+        return f"Timer({duration})"
 
-        if dynamic_units:
-            if duration < 1:
-                duration_text = f"{duration*1000:.0f}msec"
-            elif duration < 60:
-                duration_text = f"{duration:.1f}sec"
-            else:
-                duration_text = f"{duration/60:.1f}min"
-        else:
-            duration_text = f"{duration:g}sec"
+    @staticmethod
+    def _time_text(time_, time_format="%H:%M:%S"):
+        return time.strftime(time_format, time.localtime(time_))
 
-        return duration_text
+    def estimated_end_time(self, cur_progress: float):
+        time_now = time.time()
+        time_delta = time_now - self.start_time
+        time_end = time_now + time_delta / cur_progress
 
-
-def estimated_end_time(timer: Timer, progress: float, time_end_format="%H:%M:%S"):
-    time_now = time.time()
-    time_delta = time_now - timer.start_time
-    time_end = time_now + time_delta / progress
-    time_text = time.strftime(time_end_format, time.localtime(time_end))
-    return time_text
+        time_text = self._time_text(time_end)
+        return time_text
